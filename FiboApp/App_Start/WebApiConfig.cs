18fa5ef7cca42bdee96b5928fff84ad33bacadb9
B.Sane <bs.sane@e-nautia.com>
2017-12-23 15:51:17 +0100

    Creation of Fibnacci web app + the front page

---
+++ FiboApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,51 @@
+﻿using FiboApp.Configuration;
+using FiboApp.DependencyInjection;
+using Swashbuckle.Application;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+
+namespace FiboApp
+{
+    /// <summary>
+    /// WebApiConfig.
+    /// </summary>
+    public static class WebApiConfig
+    {
+
+        /// <summary>
+        /// Register.
+        /// </summary>
+        /// <param name="config"></param>
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "Swagger UI",
+                routeTemplate: string.Empty,
+                defaults: null,
+                constraints: null,
+                handler: new RedirectHandler(message => message.RequestUri.ToString().TrimEnd('/'), "swagger/ui/index"));
+
+
+            config.Services.Replace(typeof(IHttpControllerActivator), new CustomHttpControllerActivator());
+
+
+            // Configure web api to only accept an serve json
+            JsonMediaTypeFormatter jsonMediaTypeFormatter = new JsonMediaTypeFormatter();
+            var jsonSettings = jsonMediaTypeFormatter.SerializerSettings;
+            jsonSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            jsonSettings.DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Ignore;
+            jsonSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+
+            config.Formatters.Clear();
+            config.Formatters.Add(jsonMediaTypeFormatter);
+            SwaggerConfig.Register(config);
+        }
+    }
+}
